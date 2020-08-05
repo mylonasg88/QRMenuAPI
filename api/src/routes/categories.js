6af8d8bd82d987e39a2e62df8e429ef54a737a6f
George Mylonas <mylonasg88@gmail.com>
2020-08-06 00:35:59 +0800

    Mongo DB connection succeed. I had to create a user in database in mongodb in order to have access to DB.

--- api/src/routes/categories.js
+++ api/src/routes/categories.js
@@ -3,19 +3,20 @@ const {categories, categoryItems} = require("../data/categories");
 // const mongoose = require('mongoose');
 const mongoose = require('../db/index');
 // const Categories = require('../controllers/categories');
-const {Restaurant} = require('../Models/Restaurant');
+// const {Restaurant} = require('../Models/Restaurant');
 
 router.get("/", async (req, res) => {
     // const Restaurant = mongoose.model('Restaurant');
 
     try {
-        console.log(Restaurant);
-        // const restaurants = await Restaurant.find();
-        Restaurant.find({}, (err, docs) => {
-            console.log(err);
-            console.log(docs);
-            res.send([]);
-        });
+        // console.log(Restaurant);
+        // // const restaurants = await Restaurant.find();
+        // Restaurant.find({}, (err, docs) => {
+        //     console.log(err);
+        //     console.log(docs);
+        //     res.send([]);
+        // });
+        res.send([]);
 
     } catch (e) {
         console.log(e);
