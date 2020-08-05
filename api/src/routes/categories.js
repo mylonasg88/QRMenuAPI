f7371730d50d3ca53112e657cbe972525581cfb3
George Mylonas <mylonasg88@gmail.com>
2020-08-05 20:34:21 +0800

    Created connection to MySQL. Now working on Mongoose.

--- api/src/routes/categories.js
+++ api/src/routes/categories.js
@@ -1,13 +1,35 @@
 const router = require("express").Router();
-const { categories, categoryItems } = require("../data/categories");
+const {categories, categoryItems} = require("../data/categories");
+// const mongoose = require('mongoose');
+const mongoose = require('../db/index');
+// const Categories = require('../controllers/categories');
+const {Restaurant} = require('../Models/Restaurant');
 
-router.get("/", (req, res) => {
-  res.send(categories);
-  // res.send([{ id: 1, name: "Pizzas" }]);
+router.get("/", async (req, res) => {
+    // const Restaurant = mongoose.model('Restaurant');
+
+    try {
+        console.log(Restaurant);
+        // const restaurants = await Restaurant.find();
+        Restaurant.find({}, (err, docs) => {
+            console.log(err);
+            console.log(docs);
+            res.send([]);
+        });
+
+    } catch (e) {
+        console.log(e);
+        return res.send([]);
+    }
+
+});
+
+router.post('/', (req, res) => {
+
 });
 
 router.get("/items", (req, res) => {
-  res.send(categoryItems);
+    res.send(categoryItems);
 });
 
 module.exports = router;
